619fe06a21392f85d88f157d7c92475e4a88ba8c
ValentinSchmittDeer <schmitt.h.valentin@gmail.com>
2019-03-16 11:59:53 +0000

    argument parse change

--- S2_Download_FromList.py
+++ S2_Download_FromList.py
@@ -2,14 +2,14 @@
 import sys, os
 from time import strftime,strptime,localtime
 import xml.etree.ElementTree as ET
+import argparse
 from pprint import pprint
 
 #----------------------------------------------------------------------------------------------------
 # Usage
 #----------------------------------------------------------------------------------------------------
-def Usage():
-    print("""
-                Download Sentinel 2 Products on ESA Scihub
+__version__=2.3
+parser = argparse.ArgumentParser(description='''                Download Sentinel 2 Products on ESA Scihub
     I use to download Sentinel 2 data by script to get whole products or 
 just few bands. OlivierHagolle's script is an alternative for search query
 and download its.
@@ -24,11 +24,11 @@ a '.meta4' file and download content.
 example which show me an example. 
     An alternative of this script could be creating a cart on Copernicus Hub 
 to send 'aria2 -M cart.meta4' command.
+
 **************************************************************************
                              Tasks:
-Python 3
-- Search on computer a known Download Package. 
-  This is the known package list:
+Python 3 - Version %.1f
+- Search on computer a known Download Package through this list:
        • Wget : https://www.gnu.org/software/wget/
        • cURL : https://curl.haxx.se/
        • Aria2 : https://aria2.github.io/manual/en/html/index.html
@@ -36,28 +36,25 @@ Python 3
   from a txt file 'S2_DownloadProducts_IdScihub.txt' next to the script
         example: JojoId JojoPass 
 - Read a list of tiles in a ASCII file which is a kind of unheader CSV
-        TileCode ; SensingDate ; LevelOfProduct ; Bands                         ; OutputFolder
+        TileCode ; SensingDate       ; LevelOfProduct ; Bands                         ; OutputFolder
         T31TFL   ; 20181007          ; L1C            ; prod (Whole products)         ; /Users/administrateur/Downloads
         "        ; 20180925          ; L2A            ; B02B03B04 (seclected bands)   ; "
         "        ; 20180925          ; L2A            ; B05-B07 (band 5 to band 7)    ; "
-        (" can be used to repeat the same information, # for comments)
+            (" can be used to repeat the same information, # for comments)
     OR
-  Read a '.meta4' file (then OpenSearchquery avoided, band selection as hard arguments 'prodM4', download in the current directory)
+  Read a '.meta4' file (then OpenSearchquery avoided, download in the current directory)
+
 - Get tile centroides from ESA kml (hard link)
 For each tiles
-- Send an OpenSearch query to get the product ID (On Scihub, tiles are referenced by Id name)
-- Parse this query to find the right product
-- Download the product to a zip file in the output folder
+- Get the product ID by OpenSearch query (On Scihub, tiles are referenced by Id name)
+- Download the full product to a zip file
     OR
-- Download the Xml file of the product, then download bands
+  Download the product Xml file, then bands
 
 **************************************************************************
-S2_DownloadProducts_FromList.py
-Arg1: List of tiles 
-    OR
-    products.meta4 (from https://scihub.copernicus.eu)
-(The script less argument returns this help)
-""")
+
+'''% __version__,
+formatter_class=argparse.RawDescriptionHelpFormatter)
 #----------------------------------------------------------
 # List of Download Package
 #----------------------------------------------------------
@@ -68,16 +65,13 @@ dicoDP={'wget': 'wget --no-check-certificate --user={USERNAME} --password={PASSW
 #----------------------------------------------------------
 #Hard arguments
 #----------------------------------------------------------
-__version__=2.2
 # URL Kml military grid to find centroide
-urlGrid=["-https://sentinel.esa.int/documents/247904/1955685/S2A_OPER_GIP_TILPAR_MPC__20151209T095117_V20150622T000000_21000101T000000_B00.kml"]
+urlGrid=["https://sentinel.esa.int/documents/247904/1955685/S2A_OPER_GIP_TILPAR_MPC__20151209T095117_V20150622T000000_21000101T000000_B00.kml"]
 urlGrid+=['https://hls.gsfc.nasa.gov/wp-content/uploads/2016/03/S2A_OPER_GIP_TILPAR_MPC__20151209T095117_V20150622T000000_21000101T000000_B00.kml']
 # Login Scihub ID
 nameIdFile="S2_Download_IdScihub.txt"
 # URL OpenSearch Scihub API
 urlOS='https://scihub.copernicus.eu/apihub/search?q='
-# band selection during reading meta4 file
-prodM4=[2,3,4]
 
 #----------------------------------------------------------------------------------------------------
 # Hard commands
@@ -100,16 +94,32 @@ def GetDP(dicDP):
     
     del code, dicDP,
     return strDP
+
 def GetLoginId(pathFile):
     ficIn=open(pathFile)
     lstIn=ficIn.readlines()
     ficIn.close()
     
-    if not len(lstIn)==1 : raise RuntimeError("Reading error of login ID :\n%s"% ''.join(lstIn))
+    if not len(lstIn)==1 : raise RuntimeError("Login ID reading error:\n%s"% ''.join(lstIn))
     words=lstIn[0].split()
     lstId=(words[0].strip(),words[1].strip())
     
     return lstId
+
+def BandParse(text):
+    strBands=text.strip(' B').split('B')
+    lst=[]
+    for i in range(len(strBands)):
+        elem=strBands[i]
+        try:
+            lst.append(int(elem))
+        except ValueError:
+            if not elem[-1]=='-' : raise RuntimeError("Bands error: %s"% text)
+            lst+=[j for j in range(int(elem[:-1]),int(strBands[i+1]))]
+    if max(lst)>12 : raise RuntimeError("Bands list out of S2 range : %s"% '-'.join([str(elem) for elem in lst]))
+    
+    return lst
+
 def ReadListTile(pathFile):
     fileIn=open(pathFile)
     tile,date,level,bands,repOut=None,None,None,None,None
@@ -118,61 +128,54 @@ def ReadListTile(pathFile):
         clearLine=line.strip()
         if clearLine.startswith('#'): continue
         words=[word.strip() for word in clearLine.split(';')]
-        if not len(words)==5 : raise RuntimeError("Reading error of tile list : %s"% words)
+        if not len(words)==5 : raise RuntimeError("Tile list reading error: %s"% words)
         
         # Tile name
         if not words[0]=='"' : 
             val=words[0]
-            if not val[0]=='T' and len(val)==6: raise RuntimeError("Reading error of tile list : #0 = %s"% val)
+            if not val[0]=='T' and len(val)==6: raise RuntimeError("Tile list reading error: #0 = %s"% val)
             try:
                 int(val[1:3])
             except ValueError:
-                raise RuntimeError("Reading error of tile list : #0 = %s"% val)
+                raise RuntimeError("Tile list reading error: #0 = %s"% val)
             tile=val[1:]
         # Date
         if not words[1]=='"' : 
             val=words[1]
-            if not val[:2]=='20' or not len(val)==8: raise RuntimeError("Reading error of tile list : #1 = %s"% val)
+            if not val[:2]=='20' or not len(val)==8: raise RuntimeError("Tile list reading error: #1 = %s"% val)
             try:
                 date=strptime(val, '%Y%m%d')
             except ValueError:
-                raise RuntimeError("Reading error of tile list : #1 = %s"% val)
+                raise RuntimeError("Tile list reading error: #1 = %s"% val)
         # Level name
         if not words[2]=='"' : 
             val=words[2]
             level='S2MSI'+val[-2:]
             if not level=='S2MSI1C' and not level=='S2MSI2A':
-                raise RuntimeError("Reading error of tile list : #2 = %s"% val)
+                raise RuntimeError("Tile list reading error: #2 = %s"% val)
         # Bands
         if not words[3]=='"' : 
             val=words[3]
             if val=='prod':
                 bands="prod"
             else:
-                strBands=val.strip(' B').split('B')
-                bands=[]
-                for i in range(len(strBands)):
-                    elem=strBands[i]
-                    try:
-                        bands.append(int(elem))
-                    except ValueError:
-                        if not elem[-1]=='-' : raise RuntimeError("Reading error of tile list : #3 = %s"% val)
-                        bands+=[j for j in range(int(elem[:-1]),int(strBands[i+1]))]
+                bands=BandParse(val)
         # Output directory
         if not words[4]=='"' : 
             val=words[4]
-            if not os.path.isdir(val): raise RuntimeError("Reading error of tile list : #4 = %s"% val)
+            if not os.path.isdir(val): raise RuntimeError("Tile list reading error: #4 = %s"% val)
             repOut=val
         
         #final check
         if tile and date and level and bands and repOut: 
             list.append([tile,date,level,bands,repOut])
         else : 
-            raise RuntimeError("Reading error of tile list : %s"% clearLine)
+            raise RuntimeError("Tile list reading error: %s"% clearLine)
     
     fileIn.close()
     return list
-def ReadMeta4(pathFile):
+
+def ReadMeta4(pathFile,bands):
     tree=ET.parse(pathFile)
     root=tree.getroot()
     noise=root.tag.replace('}metalink','}')
@@ -181,13 +184,14 @@ def ReadMeta4(pathFile):
     for prod in root:
         title=prod.attrib['name'].replace('.zip','')
         ident=prod.find(noise+'hash').text
-        urlOD=prod.find(noise+'url').text
+        # server change dhus TO apihub 
+        urlOD=prod.find(noise+'url').text.replace('dhus','apihub')
+        
         
         words=title.split('_')
         tile=words[5][1:]
         date=words[2][:8]
         level='S2MSI'+words[1][-2:]
-        bands=prodM4
         repOut=os.curdir
         
         lst.append([tile,date,level,bands,repOut,title,ident,urlOD])
@@ -203,9 +207,8 @@ def ParseKml(lstUrlKml,listTile):
             fileKml=urllib.request.urlopen(lstUrlKml[k]).read()
         except urllib.error.URLError or ValueError:
             k+=1
-            if k>len(lstUrlKml)-1: raise RuntimeError("ESA military grid kml file does not find")
-        
-    fileKml=urllib.request.urlopen(lstUrlKml[k]).read()
+            if k>len(lstUrlKml)-1: raise RuntimeError("ESA military grid kml file did not find")
+    
     root = ET.fromstring(fileKml)
     noise=root.tag.split('}')[0]+'}'
     
@@ -220,7 +223,7 @@ def ParseKml(lstUrlKml,listTile):
     
     del fileKml,root,centerStr,center
     
-    if None in dicCenter.values(): raise RuntimeError("Tile didn't find :\n"+dicCenter)
+    if None in dicCenter.values(): raise RuntimeError("Tile did not find :"+dicCenter)
     return dicCenter
 
 def CreateOSQuery(url,name,date,level,dicoCenter):
@@ -276,15 +279,14 @@ def ReadS2XML(path,level):
 #----------------------------------------------------------
 if __name__ == "__main__":
     try:
-    #----------------------------------------------------------------------------------------------------
-    # Retrieval arguments
-    #---------------------------------------------------------------------------------------------------- 
-        print('Version:%.1f\n'% __version__)
-        nbArg = len(sys.argv)
-        if nbArg != 2: raise RuntimeError("incorrect number of arguments: %d instead of 1" % (nbArg-1))
+        #----------------------------------------------------------------------------------------------------
+        # Retrieval arguments
+        #----------------------------------------------------------------------------------------------------
+        parser.add_argument('pathIn',help='Tile list OR products.meta4 (from https://scihub.copernicus.eu)')
         
-        pathIn=sys.argv[1].strip()
-        if not os.path.isfile(pathIn): raise RuntimeError("Input list does not find")
+        parser.add_argument('-bands',default='B02B03B04',help='Set .meta4 process downloading bands (default B02B03B04)')
+        
+        args = parser.parse_args()
         
         #----------------------------------------------------------------------------------------------------
         #Unix special character
@@ -311,19 +313,19 @@ if __name__ == "__main__":
         
         lstLogin=GetLoginId(pathIdFile.strip())
         if lstLogin[0]=='Name': raise RuntimeError('Login file does not find, fill "IdScihub.txt" next to the script with ("ID Password")')
-        print("------ Hello %s ------------"% lstLogin[0])
+        print("\n------ Hello %s ---------"% lstLogin[0])
         
         #----------------------------------------------------------------------------------------------------
         # List reading
         #----------------------------------------------------------------------------------------------------
-        formIn=pathIn.split('.')[-1]
+        formIn=args.pathIn.split('.')[-1]
         if formIn=='txt':
-            lstTiles=ReadListTile(pathIn)
+            lstTiles=ReadListTile(args.pathIn)
         elif formIn=='meta4':
-            lstTiles=ReadMeta4(pathIn)
+            lstTiles=ReadMeta4(args.pathIn,args.band)
         else:
-            raise RuntimeError("Unknpwn list format : %s"% formIn)
-        print('-- %d Tiles download -----------'% len(lstTiles))
+            raise RuntimeError("Unknown list format : %s"% formIn)
+        print('-- %d Tiles -----------'% len(lstTiles))
         lstTilesUrlLess=[elem for elem in lstTiles if len(elem)<8]
         
         #----------------------------------------------------------------------------------------------------
@@ -439,8 +441,7 @@ if __name__ == "__main__":
         #----------------------------------------------------------------------------------------------------
         # End
         #----------------------------------------------------------------------------------------------------
-        print('\n\n%d Tiles to download-------------'% len(lstTiles))
-        print('%d Tiles correctly done---------------'% stat)
+        print('%d/%d Tiles correctly done---------------'% (stat/len(lstTiles)))
     
     
     #----------------------------------------------------------------------------------------------------
@@ -448,5 +449,4 @@ if __name__ == "__main__":
     #----------------------------------------------------------------------------------------------------
     except RuntimeError as msg:
         print("\nERROR - ", msg)
-        Usage()
         
